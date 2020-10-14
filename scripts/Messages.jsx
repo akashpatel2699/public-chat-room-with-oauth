32a183e4f3f92c7518a01c68fd554e771c4d1798
EC2 Default User <ec2-user@ip-172-31-69-21.ec2.internal>
2020-10-14 02:00:43 +0000

    Modify how page scroll to the bottom. Now using a div that is empty for the purpose of scrolling to bottom when prob changes

--- scripts/Messages.jsx
+++ scripts/Messages.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React , { useEffect, useRef }from 'react';
 import Message from './Message';
 
+
 const Messages = ({messages, username}) => {
+    
+    const messagesEndRef = useRef(null)
+    
+    const scrollToBottom = () => {
+        messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+    }
+
+  useEffect(scrollToBottom, [messages]);
     return (
         <div className="messages-container">
             { messages && messages.map( (message,index) => <Message username={username} key={index} message={message} />)}
+            <div ref={messagesEndRef} />
         </div>
     );
 }
